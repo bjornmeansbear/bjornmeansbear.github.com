fa9f080caa25c41b3be1feabd91fddf08cdc5f68
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-01-27 15:14:00 -0500

    .close button stopped working for some reason

--- js/ookb.js
+++ js/ookb.js
@@ -3,8 +3,9 @@
   "use strict"; // jshint ;_;
 
   // Some Custom JS for the site
-  $('.close').click(function () {
+  $(".close").click(function() {
     $("#loader").fadeOut("slow");
     });
+    
     
 }(window.jQuery);
