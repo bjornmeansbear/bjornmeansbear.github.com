78125e5602264759840930d3bb631a4f21b2320e
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-02-08 01:23:35 -0500

    things are coming together… maybe not the best idea to be doing this to my live site, but its a start

--- js/ookb.js
+++ js/ookb.js
@@ -8,12 +8,12 @@
     });
 
   // Dynamically setting colors on things based on their order?
-  $("main ul.posts li:nth-child(6n+1)").addClass("red");
-  $("main ul.posts li:nth-child(6n+2)").addClass("orange");
-  $("main ul.posts li:nth-child(6n+3)").addClass("yellow");
-  $("main ul.posts li:nth-child(6n+4)").addClass("green");
-  $("main ul.posts li:nth-child(6n+5)").addClass("blue");
-  $("main ul.posts li:nth-child(6n+6)").addClass("purple");
+  //$("main ul.posts li:nth-child(6n+1)").addClass("red");
+  //$("main ul.posts li:nth-child(6n+2)").addClass("orange");
+  //$("main ul.posts li:nth-child(6n+3)").addClass("yellow");
+  //$("main ul.posts li:nth-child(6n+4)").addClass("green");
+  //$("main ul.posts li:nth-child(6n+5)").addClass("blue");
+  //$("main ul.posts li:nth-child(6n+6)").addClass("purple");
     
     
 }(window.jQuery);
