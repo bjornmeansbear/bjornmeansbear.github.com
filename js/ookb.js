678b59100d6556d370367d5b43277fd8c4e1618a
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-02-08 01:12:25 -0500

    trying some clever JS

--- js/ookb.js
+++ js/ookb.js
@@ -6,6 +6,14 @@
   $(".close").click(function() {
     $("#loader").fadeOut("slow");
     });
+
+  // Dynamically setting colors on things based on their order?
+  $("main ul.posts li:nth-child(6n+1)").addClass(".red");
+  $("main ul.posts li:nth-child(6n+2)").addClass(".orange");
+  $("main ul.posts li:nth-child(6n+3)").addClass(".yellow");
+  $("main ul.posts li:nth-child(6n+4)").addClass(".green");
+  $("main ul.posts li:nth-child(6n+5)").addClass(".blue");
+  $("main ul.posts li:nth-child(6n+6)").addClass(".purple");
     
     
 }(window.jQuery);
